4c5ea026afe23b29e653164448fb1f1c987835b8
ar2rworld <ar2r.world@gmail.com>
2023-08-21 15:13:34 -0400

    fixing main.go

--- app/main.go
+++ app/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"errors"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
-	"strings"
-
-	"ar2rworld/golang-telegram-video-downloader/app/cleaner"
-	"ar2rworld/golang-telegram-video-downloader/app/downloader"
-	"ar2rworld/golang-telegram-video-downloader/app/httpclient"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+	"github.com/ar2rworld/golang-telegram-video-downloader/app/handler"
+	"github.com/ar2rworld/golang-telegram-video-downloader/app/httpclient"
+	"github.com/ar2rworld/golang-telegram-video-downloader/app/match"
+	"github.com/ar2rworld/golang-telegram-video-downloader/app/myerrors"
 )
 
 func main() {
@@ -21,7 +19,7 @@ func main() {
 		panic(err)
 	}
 
-	ADMIN_ID, err := strconv.ParseInt(os.Getenv("ADMIN_ID"), 10, 64)
+	adminID, err := strconv.ParseInt(os.Getenv("ADMIN_ID"), 10, 64)
 	if err != nil {
 		panic(err)
 	}
@@ -33,9 +31,23 @@ func main() {
 	updateConfig.Timeout = 30
 	updates := bot.GetUpdatesChan(updateConfig)
 
+	instagramAuthClient, err := httpclient.NewHTTPClientFromString(os.Getenv("INSTAGRAM_COOKIES_STRING"))
+	if err != nil {
+		log.Println(err)
+	}
+
 	// hello message to admin
-	helloMessage := tgbotapi.NewMessage(ADMIN_ID, "Hello, boss")
-	bot.Send(helloMessage)
+	helloMessage := tgbotapi.NewMessage(adminID, "Hello, boss")
+	sentMessage, err := bot.Send(helloMessage)
+	myerrors.CheckTextMessage(&helloMessage, err, &sentMessage)
+
+	file := "../Paradise-Coldplay.webm"
+	videoMessage := tgbotapi.NewVideo(adminID, tgbotapi.FilePath(file))
+
+	log.Println("*** Started sending video")
+	if _, err := bot.Send(videoMessage); err != nil {
+		log.Println(err)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
@@ -44,46 +56,17 @@ func main() {
 
 		messageText := update.Message.Text
 
-		isInstagramRequest := strings.Contains(messageText, "instagram.com")
+		url := match.Match(messageText)
 
-		if strings.Contains(messageText, "tiktok.com") ||
-			strings.Contains(messageText, "twitter.com") ||
-			isInstagramRequest ||
-			strings.Contains(messageText, "youtube.com/shorts") {
-			log.Println("*** Got request to download video")
-			var downloadError error
-			messageText = cleaner.CleanURL(messageText)
-
-			if isInstagramRequest && os.Getenv("INSTAGRAM_COOKIES_STRING") != "" {
-				instagramAuthClient, err := httpclient.NewHttpClientFromString(os.Getenv("INSTAGRAM_COOKIES_STRING"))
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-				downloadError = downloader.DownloadVideo(messageText, instagramAuthClient)
-			} else if isInstagramRequest {
-				downloadError = errors.New("I see that you are trying to share from instagram, but I don't have env var defined")
-			} else {
-				downloadError = downloader.DownloadVideo(messageText, &http.Client{})
-			}
-			if downloadError != nil {
-				log.Println(downloadError)
-				log.Println(update.Message)
-				continue
-			}
-			log.Println("*** Downloaded video without errors")
-			videoMessage := tgbotapi.NewVideo(update.Message.Chat.ID, tgbotapi.FilePath("./output"))
-
-			videoMessage.ReplyToMessageID = update.Message.MessageID
-
-			log.Println("*** Started sending video")
-			if _, err := bot.Send(videoMessage); err != nil {
+		if url != "" {
+			err := handler.VideoMessage(update, url, instagramAuthClient, bot)
+			if err != nil {
 				log.Println(err)
 			}
-			log.Println("*** Finished sending video")
 		} else if messageText == "osyndaisyn ba?" {
 			message := tgbotapi.NewMessage(update.Message.Chat.ID, "osyndaymyn")
-			bot.Send(message)
+			sentMessage, err := bot.Send(message)
+			myerrors.CheckTextMessage(&message, err, &sentMessage)
 		}
 	}
 }
