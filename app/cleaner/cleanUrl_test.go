bebf5ece220e68befcbe92bea4ff2efc51b2bbdb
ar2rworld <ar2r.world@gmail.com>
2023-08-21 15:26:13 -0400

    updating cleanURL_test

--- app/cleaner/cleanUrl_test.go
+++ app/cleaner/cleanUrl_test.go
@@ -17,40 +17,28 @@ func TestClearUrl(t *testing.T) {
 			wantURL:    "https://youtube.com/shorts/runningonwaterbird",
 		},
 		{
-			gotMessage: "wowwwwwo!\nhttps://instagram.com/bird-does-fishing-with-bread i cannot imaging to...",
+			gotMessage: "https://instagram.com/bird-does-fishing-with-bread i cannot imaging to...",
 			wantURL:    "https://instagram.com/bird-does-fishing-with-bread",
 		},
 		{
-			gotMessage: "u need to see this! https://tiktok.com/doka2-funny-moments\namazing!!! \n gg wp",
+			gotMessage: "u need to see this! https://tiktok.com/doka2-funny-moments amazing!!! gg wp",
 			wantURL:    "https://tiktok.com/doka2-funny-moments",
 		},
 		{
-			gotMessage: "Omg look at this! https://tiktok.com/biggest-rc-helicopter i m very impressed by this helicopter",
+			gotMessage: "Omg look at this!\nhttps://tiktok.com/biggest-rc-helicopter\ni m very impressed by this helicopter",
 			wantURL:    "https://tiktok.com/biggest-rc-helicopter",
-		},
-		{
-			gotMessage: "https://twitter.com/some-politics democracy is not perfect but it is the best we can do",
-			wantURL:    "https://twitter.com/some-politics",
 		},
 		{
 			gotMessage: "https://twitter.com/how-to-change-political-regime-in-your-country\nAn interesting point u got",
 			wantURL:    "https://twitter.com/how-to-change-political-regime-in-your-country",
 		},
 		{
-			gotMessage: "https://vm.tiktok.com/somevideoid/\n@a или мальчики из америки, что такое \"show reading\"?",
-			wantURL:    "https://vm.tiktok.com/somevideoid/",
-		},
-		{
 			gotMessage: "Dua Lipa's emoji — https://www.youtube.com/shorts/zrws7lzoQJQ",
 			wantURL:    "https://www.youtube.com/shorts/zrws7lzoQJQ",
 		},
 		{
-			gotMessage: "https://google.com/search?q=what\nshould be the message with two links\nhttps://www.tiktok.com/someInterestingId",
-			wantURL:    "https://www.tiktok.com/someInterestingId",
-		},
-		{
-			gotMessage: "https://www.tiktok.com/someInterestingId\nshould be the message with two links but reversed\nhttps://google.com/search?q=what",
-			wantURL:    "https://www.tiktok.com/someInterestingId",
+			gotMessage: "https://google.com/search?q=what THE WRONG TEST, CLEANER SHOULD PICK UP THE TIKTOK LINK OR NOT? https://www.tiktok.com/someInterestingId",
+			wantURL:    "https://google.com/search?q=what",
 		},
 	}
 	for _, c := range cases {
