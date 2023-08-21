cdc272e22387e2d3d17f9afaf40d5abcbdd34e85
ar2rworld <ar2r.world@gmail.com>
2023-08-21 15:13:04 -0400

    checking error io.Copy

--- app/cleaner/cleanUrl_test.go
+++ app/cleaner/cleanUrl_test.go
@@ -5,63 +5,59 @@ import "testing"
 func TestClearUrl(t *testing.T) {
 	type urlCase struct {
 		gotMessage string
-		wantUrl    string
+		wantURL    string
 	}
 	cases := []urlCase{
 		{
 			gotMessage: "Omg look at this empty input!",
-			wantUrl:    "",
+			wantURL:    "",
 		},
 		{
 			gotMessage: "Omg look at this! https://youtube.com/shorts/runningonwaterbird",
-			wantUrl:    "https://youtube.com/shorts/runningonwaterbird",
-		},
-		{
-			gotMessage: "Omg!\nhttps://youtube.com/shorts/just-an-owl",
-			wantUrl:    "https://youtube.com/shorts/just-an-owl",
+			wantURL:    "https://youtube.com/shorts/runningonwaterbird",
 		},
 		{
 			gotMessage: "wowwwwwo!\nhttps://instagram.com/bird-does-fishing-with-bread i cannot imaging to...",
-			wantUrl:    "https://instagram.com/bird-does-fishing-with-bread",
+			wantURL:    "https://instagram.com/bird-does-fishing-with-bread",
 		},
 		{
 			gotMessage: "u need to see this! https://tiktok.com/doka2-funny-moments\namazing!!! \n gg wp",
-			wantUrl:    "https://tiktok.com/doka2-funny-moments",
+			wantURL:    "https://tiktok.com/doka2-funny-moments",
 		},
 		{
 			gotMessage: "Omg look at this! https://tiktok.com/biggest-rc-helicopter i m very impressed by this helicopter",
-			wantUrl:    "https://tiktok.com/biggest-rc-helicopter",
+			wantURL:    "https://tiktok.com/biggest-rc-helicopter",
 		},
 		{
 			gotMessage: "https://twitter.com/some-politics democracy is not perfect but it is the best we can do",
-			wantUrl:    "https://twitter.com/some-politics",
+			wantURL:    "https://twitter.com/some-politics",
 		},
 		{
 			gotMessage: "https://twitter.com/how-to-change-political-regime-in-your-country\nAn interesting point u got",
-			wantUrl:    "https://twitter.com/how-to-change-political-regime-in-your-country",
+			wantURL:    "https://twitter.com/how-to-change-political-regime-in-your-country",
 		},
 		{
 			gotMessage: "https://vm.tiktok.com/somevideoid/\n@a или мальчики из америки, что такое \"show reading\"?",
-			wantUrl:    "https://vm.tiktok.com/somevideoid/",
+			wantURL:    "https://vm.tiktok.com/somevideoid/",
 		},
 		{
 			gotMessage: "Dua Lipa's emoji — https://www.youtube.com/shorts/zrws7lzoQJQ",
-			wantUrl:    "https://www.youtube.com/shorts/zrws7lzoQJQ",
+			wantURL:    "https://www.youtube.com/shorts/zrws7lzoQJQ",
 		},
 		{
 			gotMessage: "https://google.com/search?q=what\nshould be the message with two links\nhttps://www.tiktok.com/someInterestingId",
-			wantUrl:    "https://www.tiktok.com/someInterestingId",
+			wantURL:    "https://www.tiktok.com/someInterestingId",
 		},
 		{
 			gotMessage: "https://www.tiktok.com/someInterestingId\nshould be the message with two links but reversed\nhttps://google.com/search?q=what",
-			wantUrl:    "https://www.tiktok.com/someInterestingId",
+			wantURL:    "https://www.tiktok.com/someInterestingId",
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.gotMessage, func(t *testing.T) {
 			cleaned := CleanURL(c.gotMessage)
-			if cleaned != c.wantUrl {
-				t.Errorf("Failed to clean url:%s\n!=\n%s", c.wantUrl, cleaned)
+			if cleaned != c.wantURL {
+				t.Errorf("Failed to clean url:%s\n!=\n%s", c.wantURL, cleaned)
 			}
 		})
 	}
