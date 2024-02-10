a75acee55f9ab7d3b060ceb31b6bcac6af1416cd
ar2rworld <ar2r.world@gmail.com>
2024-02-10 17:52:41 -0500

    add MatchYoutube

--- app/match/match.go
+++ app/match/match.go
@@ -5,7 +5,18 @@ import (
 )
 
 func Match(s string) string {
-	pattern := `(https?:\/\/)?(www|vm)?\.?(youtube\.com\/shorts|twitter\.com|tiktok\.com|instagram\.com)[^\s]+`
+	pattern := `(https?:\/\/)?(www|vm)?\.?(youtube|youtu(\.com|\.be)|twitter\.com|tiktok\.com|instagram\.com)[^\s]+`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllString(s, -1)
+	if len(matches) > 0 {
+		return matches[0]
+	}
+	return ""
+}
+
+// Match only youtube video, but don't match shorts
+func Youtube(s string) string {
+	pattern := `(https?:\/\/)?(www)?\.?(youtube|youtu)(\.com|\.be)?\/([^s][^h][^o][^r][^t][^s])[^\s]+`
 	re := regexp.MustCompile(pattern)
 	matches := re.FindAllString(s, -1)
 	if len(matches) > 0 {
--- app/match/match_test.go
+++ app/match/match_test.go
@@ -14,11 +14,38 @@ func TestMatch(t *testing.T) {
 		{"Only twitter", "https://twitter.com/webflite/status/1692079842689159520?s=20", "https://twitter.com/webflite/status/1692079842689159520?s=20"},
 		{"Only youtubeshorts", "https://youtube.com/shorts/G90KEDm_G28?feature=share", "https://youtube.com/shorts/G90KEDm_G28?feature=share"},
 		{"Without https", "Some text youtube.com/shorts/G90KEDm_G28?feature=share", "youtube.com/shorts/G90KEDm_G28?feature=share"},
+		{"Short Youtube link", "Some text https://youtu.be/rfwnQzS9KkA", "https://youtu.be/rfwnQzS9KkA"},
+		{"Youtube video", "Some text https://www.youtube.com/watch?v=rfwnQzS9KkA", "https://www.youtube.com/watch?v=rfwnQzS9KkA"},
 	}
 
 	for _, matchingCase := range MatchingCases {
 		t.Run(matchingCase.Name, func(tt *testing.T) {
 			got := Match(matchingCase.Text)
+			want := matchingCase.URL
+			if got != want {
+				tt.Errorf("Couldn't match: \"%s\" -> \"%s\" , got:\"%s\"", matchingCase.Text, want, got)
+			}
+		})
+	}
+}
+
+func TestMatchYoutube(t *testing.T) {
+	MatchingCases := []struct {
+		Name string
+		Text string
+		URL  string
+	}{
+		{"Empty", "Some text", ""},
+		{"Don't match tiktok", "https://vm.tiktok.com/ZM2KGqk1v/", ""},
+		{"Only youtubeshorts", "https://youtube.com/shorts/G90KEDm_G28?feature=share", ""},
+		{"Without https", "Some text youtube.com/shorts/G90KEDm_G28?feature=share", ""},
+		{"Youtube video", "Some text https://www.youtube.com/watch?v=rfwnQzS9KkA", "https://www.youtube.com/watch?v=rfwnQzS9KkA"},
+		{"Short link", "Some text https://youtu.be/rfwnQzS9KkA", "https://youtu.be/rfwnQzS9KkA"},
+	}
+
+	for _, matchingCase := range MatchingCases {
+		t.Run(matchingCase.Name, func(tt *testing.T) {
+			got := Youtube(matchingCase.Text)
 			want := matchingCase.URL
 			if got != want {
 				tt.Errorf("Couldn't match: \"%s\" -> \"%s\" , got:\"%s\"", matchingCase.Text, want, got)
