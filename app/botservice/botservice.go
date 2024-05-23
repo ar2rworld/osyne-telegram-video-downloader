2cdd512b78e0c256d1ee165d0268949f01572387
ar2rworld <ar2r.world@gmail.com>
2024-05-23 03:46:21 -0400

    add BotService with Log to telegram channel

---
+++ app/botservice/botservice.go
@@ -0,0 +1,33 @@
+package botservice
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type BotService struct {
+	api          *tgbotapi.BotAPI
+	logChannelID int64
+}
+
+func NewBotService(api *tgbotapi.BotAPI, logChannelID int64) *BotService {
+	return &BotService{
+		api:          api,
+		logChannelID: logChannelID,
+	}
+}
+
+func (b *BotService) Log(u *tgbotapi.Update, err error) error {
+	if u.Message == nil {
+		log.Println("BotService Log used without message in update")
+		return nil
+	}
+
+	text := "Error in " + u.Message.Chat.Title + " (" + u.Message.Chat.UserName + "): " + err.Error()
+
+	msg := tgbotapi.NewMessage(b.logChannelID, text)
+	_, err = b.api.Send(msg)
+
+	return err
+}
--- app/main.go
+++ app/main.go
@@ -7,13 +7,14 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
+	"github.com/ar2rworld/golang-telegram-video-downloader/app/botservice"
 	"github.com/ar2rworld/golang-telegram-video-downloader/app/handler"
 	"github.com/ar2rworld/golang-telegram-video-downloader/app/match"
 	"github.com/ar2rworld/golang-telegram-video-downloader/app/myerrors"
 )
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+func main() { //nolint: funlen,gocyclo,cyclop
+	botAPI, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -23,21 +24,27 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	logChannelID, err := strconv.ParseInt(os.Getenv("LOG_CHANNEL_ID"), 10, 64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	cookiesPath := os.Getenv("COOKIES_PATH")
 
-	bot.Debug = false
+	botAPI.Debug = false
 
 	updateConfig := tgbotapi.NewUpdate(0)
 
 	updateConfig.Timeout = 30
-	updates := bot.GetUpdatesChan(updateConfig)
+	updates := botAPI.GetUpdatesChan(updateConfig)
 
 	// hello message to admin
 	helloMessage := tgbotapi.NewMessage(adminID, "Hello, boss")
-	sentMessage, err := bot.Send(helloMessage)
+	sentMessage, err := botAPI.Send(helloMessage)
 	myerrors.CheckTextMessage(&helloMessage, err, &sentMessage)
 
-	h := handler.NewHandler(bot)
+	h := handler.NewHandler(botAPI)
+	botService := botservice.NewBotService(botAPI, logChannelID)
 
 	for update := range updates {
 		if update.Message == nil {
@@ -48,10 +55,16 @@ func main() {
 
 		url := match.Match(messageText)
 
-		if url != "" {
+		if url != "" { //nolint: nestif
 			err := h.VideoMessage(&update, url, cookiesPath)
 			if err != nil {
 				log.Println(err)
+
+				err = botService.Log(&update, err)
+				if err != nil {
+					log.Println(err)
+				}
+
 				err = h.ThumbDown(&update)
 				if err != nil {
 					log.Println("Error while reacting:", err)
@@ -59,7 +72,7 @@ func main() {
 			}
 		} else if messageText == "osyndaisyn ba?" {
 			message := tgbotapi.NewMessage(update.Message.Chat.ID, "osyndaymyn")
-			sentMessage, err := bot.Send(message)
+			sentMessage, err := botAPI.Send(message)
 			myerrors.CheckTextMessage(&message, err, &sentMessage)
 		}
 	}
