277ad3e423cfe0875619728b2e72f2d690ce0246
ar2rworld <ar2r.world@gmail.com>
2025-08-28 13:32:28 +0500

    add RemuxToMP4

--- app/downloader/converter.go
+++ app/downloader/converter.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func Convert(ctx context.Context, path string) (string, error) {
@@ -12,6 +13,33 @@ func Convert(ctx context.Context, path string) (string, error) {
 	cmd := exec.CommandContext(ctx, "ffmpeg", "-y", "-i", path, "-b:v", "800k", "-c:v", "libx264", "-c:a", "aac", "-b:a", "128k", outputFile)
 	cmd.Stderr = os.Stderr
 
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return outputFile, nil
+}
+
+func RemuxToMP4(ctx context.Context, path string) (string, error) {
+	// Change extension to .mp4
+	outputFile := filepath.Base(path) + ".mp4"
+
+	cmd := exec.CommandContext(ctx,
+		"ffmpeg",
+		"-y",       // overwrite without asking
+		"-i", path, // input file
+		"-c:v", "copy", // copy video stream
+		"-c:a", "copy", // copy audio stream
+		"-movflags", "+faststart", // move mp4 metadata to beginning
+		"-avoid_negative_ts", "make_zero", // adjust timestamps
+		outputFile,
+	)
+
+	// Pipe ffmpeg output to current stderr/stdout
+	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 
 	err := cmd.Run()
