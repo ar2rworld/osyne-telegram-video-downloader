8e71d5c5cfabe57d6f3ff5257c89f86549d0c6be
ar2rworld <ar2r.world@gmail.com>
2025-11-03 17:37:32 +0500

    add constants

---
+++ app/constants/constants.go
@@ -0,0 +1,19 @@
+package constants
+
+const TgUploadLimit = 50
+
+const (
+	MinHeight    = 300
+	MinWidth     = 600
+	BytesInKByte = 1024
+)
+
+const (
+	AudioCodec    = "mp4a"
+	VideoCodec    = "avc1"
+)
+
+const (
+	HalfMinute      = 30
+	SecondsInMinute = 60
+)
