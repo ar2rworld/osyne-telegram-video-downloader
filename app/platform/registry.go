dbfe74e21378e2bcecbbefc9d339c89803557696
ar2rworld <ar2r.world@gmail.com>
2025-11-04 11:42:33 +0500

    add platform registry

---
+++ app/platform/registry.go
@@ -0,0 +1,22 @@
+package platform
+
+type Registry struct {
+	platforms []Platform
+}
+
+func NewRegistry() *Registry {
+	return &Registry{}
+}
+
+func (r *Registry) Register(p Platform) {
+    r.platforms = append(r.platforms, p)
+}
+
+func (r *Registry) FindPlatform(url string) Platform {
+	for _, p := range r.platforms {
+		if p.Match(url) {
+			return p
+		}
+	}
+	return nil
+}
