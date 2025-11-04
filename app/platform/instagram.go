e544bc0f5299a72b2686ed12197ba4cae9fbe4a6
ar2rworld <ar2r.world@gmail.com>
2025-11-04 11:47:00 +0500

    add instagram platform

---
+++ app/platform/instagram.go
@@ -0,0 +1,48 @@
+package platform
+
+import (
+	c "github.com/ar2rworld/golang-telegram-video-downloader/app/constants"
+	"github.com/ar2rworld/golang-telegram-video-downloader/app/match"
+	"github.com/wader/goutubedl"
+)
+
+type Instagram struct {
+	cookiesPath string
+}
+
+// RemuxRequired implements Platform.
+func (i *Instagram) RemuxRequired() bool {
+	return false
+}
+
+// NeedCut implements Platform.
+func (i *Instagram) NeedCut(*goutubedl.Result) (bool, error) {
+	return false, nil
+}
+
+func NewInstagram(cookiesPath string) *Instagram {
+	return &Instagram{cookiesPath: cookiesPath}
+}
+
+// MaxDuration implements Platform.
+func (i *Instagram) MaxDuration(*goutubedl.Result) (string, error) {
+	return c.DefaultSections, nil
+}
+
+// SelectFormat implements Platform.
+func (i *Instagram) SelectFormat(formats []goutubedl.Format) (string, error) {
+	return c.BestFormat, nil
+}
+
+func (i *Instagram) Name() string {
+	return "instagram"
+}
+
+func (i *Instagram) Match(url string) bool {
+	return match.Instagram(url) != ""
+}
+
+func (i *Instagram) ConfigureDownload(url string, opts *goutubedl.Options) error {
+	opts.Cookies = i.cookiesPath
+	return nil
+}
