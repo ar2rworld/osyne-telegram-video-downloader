8f3b89c10eec2354e50f3d4a26f3f30f052fb611
ar2rworld <ar2r.world@gmail.com>
2025-11-09 11:18:50 +0500

    add myerrors classification

---
+++ app/myerrors/classification.go
@@ -0,0 +1,17 @@
+package myerrors
+
+type ErrorSeverity int
+
+const (
+	SeverityUser       ErrorSeverity = iota // Пользователь может исправить (плохой URL)
+	SeverityTransient                       // Возможен retry (сеть)
+	SeverityMaintainer                      // Требуется действие админа (cookies)
+	SeverityCritical                        // Сбой системы
+)
+
+type ClassifiedError interface {
+	error
+	Severity() ErrorSeverity
+	UserMessage() string       // Безопасное сообщение для пользователя
+	MaintainerMessage() string // Детальное сообщение для логов/алертов
+}
